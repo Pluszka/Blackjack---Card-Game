2ad457a95f9875bef87b54e3e84b8d4e56dccc29
KarolinaKoza <97195524+Pluszka@users.noreply.github.com>
2022-01-14 22:24:27 +0000

    prompting cards starting values

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ def deal():
   computer=choice(cards),choice(cards)
   return player, computer
 
-def
+dealt_cards=deal()
+player_cards=dealt_cards[0]
+computer_cards=dealt_cards[1]
+card_to_show=computer_cards[1],'?'
+print(f'Here you are your cards: {player_cards}\nThat\'s a single card belong to your opponent: {card_to_show}')
