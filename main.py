a2141f9c6d49dd9649c7e24d72cab5c117e5c6bb
KarolinaKoza <97195524+Pluszka@users.noreply.github.com>
2022-01-14 22:11:08 +0000

    created deal function

---
+++ main.py
@@ -0,0 +1,8 @@
+from random import choice
+cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+def deal():
+  player=choice(cards),choice(cards)
+  computer=choice(cards),choice(cards)
+  return player, computer
+
+def
