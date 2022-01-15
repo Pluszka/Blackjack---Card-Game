adf66d2a9627abca26aafdb291f5fbd7607d6213
KarolinaKoza <97195524+Pluszka@users.noreply.github.com>
2022-01-15 11:48:49 +0000

    Dealing cards is now more versatile (deal takes arguments )

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from random import choice
 cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
-def deal():
-  player=choice(cards),choice(cards)
-  computer=choice(cards),choice(cards)
+def deal(amount_player, amount_computer):
+  player=[choice(cards) for card in range(amount_player)]
+  computer=[choice(cards) for card in range(amount_computer)]
   return player, computer
 
-dealt_cards=deal()
+
+dealt_cards=deal(2,2)
 player_cards=dealt_cards[0]
 computer_cards=dealt_cards[1]
 card_to_show=computer_cards[1],'?'
-print(f'Here you are your cards: {player_cards}\nThat\'s a single card belong to your opponent: {card_to_show}')
+player_score=(sum(player_cards))
+computer_score=(sum(computer_cards))
+
+print(f'Here you are your cards: {player_cards}\nThat\'s a single card which belong to your opponent: {card_to_show}')
+if player_score>=21:
+  print(f'Your score is equal to {player_score}, you lose.')
+else:
+  next_cards=input(f'Your score is equal to {player_score}.\nWill you continue?(Y/N)').upper()
+  if next_cards=='Y':
+    player_cards+=deal(1,0)
